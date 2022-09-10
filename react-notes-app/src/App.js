7cdcebcb5f50e8c572ce6fef15604c5b5b547932
Sahil <sahilnalavade25@gmail.com>
2022-09-11 03:57:58 +0530

    First Commit

---
+++ react-notes-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NoteList from "./components/NotesList";
+const App=() => {
+  return(
+    <div className="container">
+      <NoteList />
+
+    </div>
+  )
+};
+
+export default App;
---
+++ react-notes-app/src/components/NotesList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Note from "./Note"; 
+
+const NotesList=()=> {
+    return <div className='notes-List'>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+
+    </div>
+}
+
+export default NotesList
